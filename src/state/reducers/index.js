071a5882aa914dbce43aba6804bceb7a19c4b219
mikelsito <mikelsito@gmail.com>
2018-08-01 12:12:00 -0500

    added import character reducer to index.js in state/reducers

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,5 +1,6 @@
 // All various reducers you add will all be combined in rootReducer.js
 import { combineReducers } from 'redux'
+import character from 'state/reducers/character'
 
 export default combineReducers({
   character
