45c6ba82908b352a3b0f3f8df55b6b175df4096e
mikelsito <mikelsito@gmail.com>
2018-08-01 12:22:12 -0500

    edits fixed previous error, introduced new error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { createStore, applyMiddleware} from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import reducer from 'state/reducers'
+import reducer from './state/reducers'
 import "./stylesheets/application.css"
 
 const store = createStore(reducer)
--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,6 @@
 // All various reducers you add will all be combined in rootReducer.js
 import { combineReducers } from 'redux'
-import character from 'state/reducers/character'
+import character from './character'
 
 export default combineReducers({
   character
