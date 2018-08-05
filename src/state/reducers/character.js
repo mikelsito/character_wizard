73dd4e1590f7bc033872929e3d3f9c64e66b30bb
mikelsito <mikelsito@gmail.com>
2018-08-04 22:39:39 -0500

    Attempt at component with redux, mucho bugs

--- src/state/reducers/character.js
+++ src/state/reducers/character.js
@@ -1,9 +1,28 @@
 import {
-  SET_RACE
+  SET_RACE,
+  SET_CLASS
 } from '../actions'
 
 export const initialState = {
-  race: null
+  id: null,
+  username: null,
+  race: null,
+  class: null,
+  background: null,
+  alignment: null,
+  characterName: null,
+  height: null,
+  weight: null,
+  size: null,
+  speed: null,
+  abilityModifiers: {
+    strength: [],
+    dexterity: [],
+    constitution: [],
+    intelligence: [],
+    wisdom: [],
+    charsima: []
+  }
 }
 
 export default (state = initialState, action) => {
@@ -13,7 +32,12 @@ export default (state = initialState, action) => {
         ...state,
         race: action.payload.race
       }
-      default:
-        return state
+    case SET_CLASS:
+      return {
+        ...state,
+        class: action.payload.class
+      }
+    default:
+      return state
   }
 }
