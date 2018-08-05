064a6c738b63cb14a2f7a99034a3df98b83016f5
Conor Davidson <conor@conordavidson.com>
2018-08-05 00:06:01 -0400

    Race and class form submission.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
-import { createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 // import thunk from 'redux-thunk'
 import reducer from 'state/reducers'
 import "./stylesheets/application.css"
 import Stage1 from "components/Stages/Stage1"
 
-const store = createStore(reducer)
+// I added redux dev tools extension so you can see if the actions work!
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <Stage1></Stage1>
+          <Stage1/>
         </div>
       </Provider>
     );
