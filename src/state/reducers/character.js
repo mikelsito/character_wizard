b56903bc484c2ae9ce5bf31b36862ef0eba98bd2
Mike Corbett <srcorbetto@gmail.com>
2018-08-15 16:27:53 -0500

    navbar started

--- src/state/reducers/character.js
+++ src/state/reducers/character.js
@@ -11,10 +11,6 @@ export const initialState = {
   subclass: null,
   background: null,
   alignment: null,
-<<<<<<< HEAD
-=======
-  // alignmentDescription: null,
->>>>>>> Added navbar
   characterName: null,
   level: null,
   height: null,
@@ -23,18 +19,6 @@ export const initialState = {
   size: null,
   speed: null,
   languages: null,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  languageOptions: null,
-=======
-  languageDescription: null,
->>>>>>> working on what to display on left
->>>>>>> working on what to display on left
-=======
-  // languageDescriptions: null,
->>>>>>> Added navbar
   abilityBonuses: null,
   savingThrows: null,
   proficiencies: null,
@@ -51,22 +35,8 @@ export default (state = initialState, action) => {
         speed: action.payload.speed,
         size: action.payload,
         abilityBonuses: action.payload.ability_bonuses,
-<<<<<<< HEAD
-=======
-        // ageDescription: action.payload.age,
-        // alignmentDescription: action.payload.alignment,
->>>>>>> Added navbar
         size: action.payload.size,
         languages: action.payload.languages,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        languageOptions: action.payload.language_options,
->>>>>>> working on what to display on left
-=======
-        languageDescriptions: action.payload.language_desc,
->>>>>>> Added navbar
         traits: action.payload.traits
       }
     case SET_CLASS: 
@@ -75,20 +45,6 @@ export default (state = initialState, action) => {
         class: action.payload.name,
         hitDie: action.payload.hit_die,
         savingThrows: action.payload.saving_throws,
-<<<<<<< HEAD
-=======
-        startingEquipmentUrl: action.payload.starting_equipment,
-        classLvlUrl: action.payload.classLvlUrl,
-        subclasses: action.payload.subclasses
-<<<<<<< HEAD
-=======
-        languageDescription: action.payload.language_desc,
-        traits: action.payload.traits,
-        raceRefUrl: action.payload.url
->>>>>>> working on what to display on left
->>>>>>> working on what to display on left
-=======
->>>>>>> Added navbar
       }
     default:
       return state
